2e62c70387f0c5d64e4abae2b3e4405c14e74942
Fernando C <fndo.cabrera92@gmail.com>
2020-10-29 16:47:35 -0300

    Se agrega Week 1.py

---
+++ scripts/Week 1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  # This notebook will be used for the IBM course Capstone Project
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
